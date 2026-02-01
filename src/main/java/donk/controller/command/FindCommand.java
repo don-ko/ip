b8597738b2fc19598ba3a5809a253e836436a184
don-ko <donaldkong1137@gmail.com>
2026-02-01 14:01:37 +0800

    docs: Add FindCommand documentation

--- src/main/java/donk/controller/command/FindCommand.java
+++ src/main/java/donk/controller/command/FindCommand.java
@@ -3,13 +3,30 @@ package donk.controller.command;
 import donk.model.task.Task;
 import donk.model.task.TaskList;
 
+/**
+ * Finds tasks in a {@link TaskList} whose description matches the given search text.
+ */
 public class FindCommand extends Command {
+    /**
+     * Raw search text provided by the user.
+     */
     private final String searchString;
 
+    /**
+     * Creates a command that searches for tasks containing the given text.
+     *
+     * @param searchString user-provided text to search for
+     */
     public FindCommand(String searchString) {
         this.searchString = searchString;
     }
 
+    /**
+     * Executes the search and formats the matching tasks using 1-based indices.
+     *
+     * @param tasks task list to search
+     * @return formatted search result message
+     */
     @Override
     public String execute(TaskList tasks) {
         StringBuilder sb = new StringBuilder("found matching tasks!\n");
